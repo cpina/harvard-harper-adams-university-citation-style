3ff09a6ed1e6620be07d83ffe7999b8227b730e3
Carles Pina i Estany <carles@pina.cat>
2020-11-11 09:32:32 +0000

    Tooling for first comparison

--- build.sh
+++ build.sh
@@ -3,14 +3,24 @@
 set -e
 set -u
 
+mkdir -p tmp
+output_file="tmp/bibliography-actual.html"
+expected_file="bibliography-expected.html"
+
 pandoc --pdf-engine=xelatex \
 	--bibliography test_bibliography_ha_style.bib \
 	--csl /usr/share/citation-style-language/styles/harvard-anglia-ruskin-university.csl \
 	-f markdown example-citations.md \
-	-s -o tmp/example-citations.odt
+	-o "$output_file"
 
-#cd tmp
-#pdflatex example-citations.tex
-#pdflatex example-citations.tex
-#pdflatex example-citations.tex
-#pdflatex example-citations.tex
+echo "$output_file done"
+if cmp "$output_file" "$expected_file"
+then
+	echo "Same files!"
+else
+	echo "Some differences"
+	echo
+	echo "diff -u "$output_file" "$expected_file"| ydiff -s --wrap"
+	echo
+	echo "vimdiff +\"windo set wrap\" \"$output_file\" \"$expected_file\""
+fi
--- example-citations.md
+++ example-citations.md
@@ -1,4 +1,5 @@
-As someone @RefWorks:doc:5fa9c88fe4b035e8035d4838 said this is important.
-
 ::: {#refs}
 :::
+---
+nocite: |
+  @RefWorks:doc:5fa9c88fe4b035e8035d4838

04ae20b4a50f472b2692c27d0b102b34d01d70c9
Carles Pina i Estany <carles@pina.cat>
2020-11-13 08:54:17 +0000

    Improve script to compare generated vs expected

--- build.sh
+++ build.sh
@@ -3,39 +3,39 @@
 set -e
 set -u
 
+BIBTEX_FILE="test_bibliography_ha_style_examples.bib"
+
 mkdir -p tmp
-output_file="tmp/bibliography-actual.html"
-expected_file="bibliography-expected.html"
+OUTPUT_HTML_FILE="tmp/bibliography-actual.html"
+OUTPUT_PDF_FILE="tmp/bibliography-actual.pdf"
+OUTPUT_MARKDOWN_FILE="tmp/bibliography-actual.md"
+EXPECTED_FILE="bibliography-expected.html"
 
 # removes urls if they are tagged as no-urls
-./filter_urls.py test_bibliography_ha_style_examples.bib test_bibliography_ha_style_no_urls.bib
+./filter_urls.py "$BIBTEX_FILE" test_bibliography_ha_style_no_urls.bib
 
-pandoc --pdf-engine=xelatex \
-	--bibliography test_bibliography_ha_style_no_urls.bib \
-	--csl harvard-harper-adams-university.csl \
-	-f markdown example-citations.md \
-	-o "tmp/bibliography-actual.pdf"
+for OUTPUT_FILE in "$OUTPUT_HTML_FILE" "$OUTPUT_PDF_FILE" "$OUTPUT_MARKDOWN_FILE"
+do
+	pandoc --pdf-engine=xelatex \
+		--bibliography test_bibliography_ha_style_no_urls.bib \
+		--csl harvard-harper-adams-university.csl \
+		-f markdown example-citations.md \
+		-o "$OUTPUT_FILE"
+	echo "Generated file: $OUTPUT_FILE"
+done
 
-pandoc --pdf-engine=xelatex \
-	--bibliography test_bibliography_ha_style_no_urls.bib \
-	--csl harvard-harper-adams-university.csl \
-	-f markdown example-citations.md \
-	-o "tmp/bibliography-actual.md"
+echo
+echo "Expected file : $EXPECTED_FILE"
 
-pandoc --pdf-engine=xelatex \
-	--bibliography test_bibliography_ha_style_no_urls.bib \
-	--csl harvard-harper-adams-university.csl \
-	-f markdown example-citations.md \
-	-o "$output_file"
-
-echo "$output_file done"
-if cmp "$output_file" "$expected_file"
+if cmp "$OUTPUT_HTML_FILE" "$EXPECTED_FILE" > /dev/null
 then
-	echo "Same files!"
+	echo
+	echo "PASS! (generated and expected HTML files are the same)"
 else
-	echo "Some differences"
 	echo
-	echo "diff -u "$output_file" "$expected_file"| ydiff -s --wrap"
+	echo "FAILED! (generated and expected HTML are not the same)"
 	echo
-	echo "vimdiff +\"windo set wrap\" \"$output_file\" \"$expected_file\""
+	# echo "diff -u "$output_file" "$expected_file"| ydiff -s --wrap"
+	echo "See differences with:"
+	echo "vimdiff +\"windo set wrap\" \"$(pwd)/$OUTPUT_HTML_FILE\" \"$(pwd)/$EXPECTED_FILE\""
 fi
